9666ff9bdf6d6bdaeb73f70176f8b9840b7afd1f
leeyejin <leeyejin113@gmail.com>
2024-01-05 21:59:22 +0900

    Feat: add sample validator

---
+++ src/main/java/sparta/com/sappun/global/validator/SampleValidator.java
@@ -0,0 +1,18 @@
+package sparta.com.sappun.global.validator;
+
+import static sparta.com.sappun.global.ResultCode.NOT_FOUND_SAMPLE;
+
+import sparta.com.sappun.domain.sample.entity.Sample;
+import sparta.com.sappun.global.exception.GlobalException;
+
+public class SampleValidator {
+    public static void validate(Sample sample) {
+        if (isNullSample(sample)) {
+            throw new GlobalException(NOT_FOUND_SAMPLE);
+        }
+    }
+
+    private static boolean isNullSample(Sample sample) {
+        return sample == null;
+    }
+}
