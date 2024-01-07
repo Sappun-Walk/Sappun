26a4d4e14c05431df34bd5c8c1c6166ce3505523
leeyejin <leeyejin113@gmail.com>
2024-01-07 20:30:43 +0900

    [#2] Feat: add RedisUtil

---
+++ src/main/java/sparta/com/sappun/global/redis/RedisUtil.java
@@ -0,0 +1,34 @@
+package sparta.com.sappun.global.redis;
+
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.stereotype.Component;
+
+@Slf4j(topic = "Redis Util")
+@Component
+@RequiredArgsConstructor
+public class RedisUtil {
+
+    public static final int REFRESH_TOKEN_EXPIRED_TIME = 60 * 24 * 14; // 2주
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void set(String key, Object o, int minutes) {
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(o.getClass()));
+        redisTemplate.opsForValue().set(key, o, minutes, TimeUnit.MINUTES);
+    }
+
+    public Object get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public boolean delete(String key) {
+        return Boolean.TRUE.equals(redisTemplate.delete(key));
+    }
+
+    public boolean hasKey(String key) {
+        return Boolean.TRUE.equals(redisTemplate.hasKey(key));
+    }
+}
