1bc6c035a382aad546f175daa0ec3729d9555ffd
leeyejin <leeyejin113@gmail.com>
2024-01-07 15:52:27 +0900

    [#3] Test: add repository test

---
+++ src/test/java/sparta/com/sappun/domain/user/repository/UserRepositoryTest.java
@@ -0,0 +1,35 @@
+package sparta.com.sappun.domain.user.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import sparta.com.sappun.domain.sample.entity.Sample;
+import sparta.com.sappun.domain.user.entity.User;
+import sparta.com.sappun.test.UserTest;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class UserRepositoryTest implements UserTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("save 테스트")
+    void saveTest() {
+        // given
+
+        // when
+        User saveUser = userRepository.save(TEST_USER);
+
+        // then
+        assertEquals(saveUser.getUsername(), TEST_USER_USERNAME);
+        assertEquals(saveUser.getNickname(), TEST_USER_NICKNAME);
+    }
+}
