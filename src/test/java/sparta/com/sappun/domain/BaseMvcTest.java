bbe2c6328911de51f4493ec59dc9f64e91b41c80
leeyejin <leeyejin113@gmail.com>
2024-01-08 22:20:54 +0900

    [#11] Test: modify BaseMvcTest

--- src/test/java/sparta/com/sappun/domain/BaseMvcTest.java
+++ src/test/java/sparta/com/sappun/domain/BaseMvcTest.java
@@ -1,6 +1,7 @@
 package sparta.com.sappun.domain;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static sparta.com.sappun.test.UserTest.TEST_USER;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.security.Principal;
@@ -8,10 +9,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import sparta.com.sappun.global.security.UserDetailsImpl;
 
 @ExtendWith({MockitoExtension.class, SpringExtension.class})
 public class BaseMvcTest {
@@ -23,18 +27,18 @@ public class BaseMvcTest {
     @Autowired private WebApplicationContext context;
 
     @BeforeEach
-    public void setUp() throws Exception {
+    public void setUp() {
         mockMvc =
                 MockMvcBuilders.webAppContextSetup(context)
                         .apply(springSecurity(new MockSpringSecurityFilter()))
                         .build();
-        //        mockUserSetup();
+        mockUserSetup();
     }
 
-    //    private void mockUserSetup() {
-    //        UserDetails testUserDetails = new UserDetailsImpl(TEST_USER);
-    //        mockPrincipal =
-    //            new UsernamePasswordAuthenticationToken(
-    //                testUserDetails, "", testUserDetails.getAuthorities());
-    //    }
+    private void mockUserSetup() {
+        UserDetails testUserDetails = new UserDetailsImpl(TEST_USER);
+        mockPrincipal =
+                new UsernamePasswordAuthenticationToken(
+                        testUserDetails, "", testUserDetails.getAuthorities());
+    }
 }
