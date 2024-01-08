5bd12e0d1de1ab8e0f13cad625263be2a9f17623
leeyejin <leeyejin113@gmail.com>
2024-01-08 16:57:50 +0900

    [#11] Test: add repository test

--- src/test/java/sparta/com/sappun/domain/user/controller/UserControllerTest.java
+++ src/test/java/sparta/com/sappun/domain/user/controller/UserControllerTest.java
@@ -13,8 +13,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import sparta.com.sappun.domain.BaseMvcTest;
+import sparta.com.sappun.domain.user.dto.request.UserLoginReq;
 import sparta.com.sappun.domain.user.dto.request.UserSignupReq;
+import sparta.com.sappun.domain.user.dto.response.UserLoginRes;
 import sparta.com.sappun.domain.user.dto.response.UserSignupRes;
+import sparta.com.sappun.domain.user.entity.Role;
 import sparta.com.sappun.domain.user.service.UserService;
 import sparta.com.sappun.global.jwt.JwtUtil;
 import sparta.com.sappun.global.redis.RedisUtil;
@@ -48,6 +51,39 @@ class UserControllerTest extends BaseMvcTest implements UserTest {
         mockMvc
                 .perform(
                         post("/api/users/signup")
+                                .content(objectMapper.writeValueAsString(req))
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("login 테스트")
+    void loginTest() throws Exception {
+        // given
+        UserLoginReq req =
+                UserLoginReq.builder().username(TEST_USER_USERNAME).password(TEST_USER_PASSWORD).build();
+
+        UserLoginRes res =
+                UserLoginRes.builder()
+                        .id(TEST_USER_ID)
+                        .username(TEST_USER_USERNAME)
+                        .nickname(TEST_USER_NICKNAME)
+                        .role(Role.USER)
+                        .build();
+
+        String accessToken = "accessToken";
+        String refreshToken = "refreshToken";
+
+        // when
+        when(userService.login(any())).thenReturn(res);
+        when(jwtUtil.createAccessToken(any(), any())).thenReturn(accessToken);
+        when(jwtUtil.createAccessToken(any(), any())).thenReturn(refreshToken);
+
+        // then
+        mockMvc
+                .perform(
+                        post("/api/users/login")
                                 .content(objectMapper.writeValueAsString(req))
                                 .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
