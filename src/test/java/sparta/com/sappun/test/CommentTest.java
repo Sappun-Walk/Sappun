f51f615db8c869b891e85b11c7ee00170d06d195
hayul93 <parksy785@gmail.com>
2024-01-09 18:59:06 +0900

    [#5] Refator: UserTest 상속

--- src/test/java/sparta/com/sappun/test/CommentTest.java
+++ src/test/java/sparta/com/sappun/test/CommentTest.java
@@ -2,17 +2,16 @@ package sparta.com.sappun.test;
 
 import sparta.com.sappun.domain.comment.entity.Comment;
 
-public interface CommentTest {
+public interface CommentTest extends UserTest{
 
     Long TEST_COMMENT_ID = 1L;
-    String TEST_COMMENT_NICKNAME = "nickname";
     String TEST_COMMENT_CONTENT = "test content";
     String TEST_COMMENT_FILEURL = "TEST URL";
 
     Comment TEST_COMMENT =
             Comment.builder()
                     .id(TEST_COMMENT_ID)
-                    .nickname(TEST_COMMENT_NICKNAME)
+                    .nickname(TEST_USER_NICKNAME)
                     .content(TEST_COMMENT_CONTENT)
                     .fileUrl(TEST_COMMENT_FILEURL)
                     .build();
