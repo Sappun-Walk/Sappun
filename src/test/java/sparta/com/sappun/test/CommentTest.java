cd8be80147efb16730077aaff12298fc377b2745
hayul93 <parksy785@gmail.com>
2024-01-09 15:22:55 +0900

    [#5] Test: add service Test

--- src/test/java/sparta/com/sappun/test/CommentTest.java
+++ src/test/java/sparta/com/sappun/test/CommentTest.java
@@ -5,8 +5,15 @@ import sparta.com.sappun.domain.comment.entity.Comment;
 public interface CommentTest {
 
     Long TEST_COMMENT_ID = 1L;
+    String TEST_COMMENT_NICKNAME = "nickname";
     String TEST_COMMENT_CONTENT = "test content";
+    String TEST_COMMENT_FILEURL = "TEST URL";
 
     Comment TEST_COMMENT =
-            Comment.builder().id(TEST_COMMENT_ID).content(TEST_COMMENT_CONTENT).build();
+            Comment.builder()
+                    .id(TEST_COMMENT_ID)
+                    .nickname(TEST_COMMENT_NICKNAME)
+                    .content(TEST_COMMENT_CONTENT)
+                    .fileUrl(TEST_COMMENT_FILEURL)
+                    .build();
 }
