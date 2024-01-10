a72ab5487012f11af4a372cdd39040ac416c80d0
rlawogks22 <rlawogks1234@gmail.com>
2024-01-10 18:22:10 +0900

    [#35] build:빌드 오류 수정

--- src/main/java/sparta/com/sappun/domain/LikeComment/contorller/LikeCommentLikeController.java
+++ src/main/java/sparta/com/sappun/domain/LikeComment/contorller/LikeCommentLikeController.java
@@ -16,7 +16,6 @@ import sparta.com.sappun.global.security.UserDetailsImpl;
 @RequestMapping("/api/comments")
 public class LikeCommentLikeController {
     private final LikeCommentService likeCommentService;
-
     @PostMapping("/{commentId}/like")
     public CommonResponse<LikeCommentSaveRes> likeComment(@PathVariable Long commentId,
                                                            @AuthenticationPrincipal UserDetailsImpl userId) {
