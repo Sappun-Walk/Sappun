83c8ff6f72386fb5091beb88f4c759758d28fde5
jump6923 <jump6923@gmail.com>
2024-01-10 10:28:36 +0900

    [#12] Fix: boardList get api

---
+++ src/main/java/sparta/com/sappun/domain/board/repository/BoardRepository.java
@@ -0,0 +1,19 @@
+package sparta.com.sappun.domain.board.repository;
+
+import org.springframework.data.repository.RepositoryDefinition;
+import sparta.com.sappun.domain.board.entity.Board;
+import sparta.com.sappun.domain.board.entity.RegionEnum;
+
+import java.util.List;
+
+@RepositoryDefinition(domainClass = Board.class, idClass = long.class)
+public interface BoardRepository {
+
+    Board findById(Long boardId);
+
+    List<Board> findAllByRegion(RegionEnum region);
+
+    Board save(Board board);
+
+    void delete(Board board);
+}
