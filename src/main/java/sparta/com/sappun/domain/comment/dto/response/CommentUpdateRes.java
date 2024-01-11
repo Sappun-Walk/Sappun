d52cb7518c354b74c30a9af49eef8d66284287e1
hayul93 <parksy785@gmail.com>
2024-01-11 14:47:04 +0900

    [#7] Refactor: private으로 수정

--- src/main/java/sparta/com/sappun/domain/comment/dto/response/CommentUpdateRes.java
+++ src/main/java/sparta/com/sappun/domain/comment/dto/response/CommentUpdateRes.java
@@ -14,7 +14,7 @@ public class CommentUpdateRes {
     private String fileUrl;
 
     @Builder
-    public CommentUpdateRes(Long id, String nickname, String content, String fileUrl) {
+    private CommentUpdateRes(Long id, String nickname, String content, String fileUrl) {
         this.id = id;
         this.nickname = nickname;
         this.content = content;
