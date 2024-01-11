564d19c330e2b7275fcda0c0d7bf06df331eee1d
rlawogks22 <rlawogks1234@gmail.com>
2024-01-11 22:10:47 +0900

    [#48] test:댓글 신고 테스트

--- src/test/java/sparta/com/sappun/domain/ReportComment/service/ReportCommentServiceTest.java
+++ src/test/java/sparta/com/sappun/domain/ReportComment/service/ReportCommentServiceTest.java
@@ -1,36 +1,34 @@
 package sparta.com.sappun.domain.ReportComment.service;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.times;
 
-import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import sparta.com.sappun.domain.ReportComment.repository.ReportCommentRepository;
 import sparta.com.sappun.domain.comment.repository.CommentRepository;
 import sparta.com.sappun.domain.user.repository.UserRepository;
 import sparta.com.sappun.test.ReportCommentTest;
 
-@SpringBootTest
-@Transactional
+@ExtendWith(MockitoExtension.class)
 class ReportCommentServiceTest implements ReportCommentTest {
 
-    @MockBean private CommentRepository commentRepository;
+    @Mock private CommentRepository commentRepository;
 
-    @MockBean private UserRepository userRepository;
+    @Mock private UserRepository userRepository;
 
-    @MockBean private ReportCommentRepository reportCommentRepository;
+    @Mock private ReportCommentRepository reportCommentRepository;
 
-    @Autowired private ReportCommentService reportCommentService;
+    @InjectMocks private ReportCommentService reportCommentService;
 
     @Test
     @DisplayName("댓글 신고 테스트")
-    public void reportCommentRes() {
+    void reportCommentRes() {
         // given
         when(userRepository.findById(any())).thenReturn(TEST_USER);
         when(commentRepository.findById(any())).thenReturn(TEST_COMMENT);
