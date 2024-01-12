3bf74eff2c0fb3b3f2def643d94f1a75e5b162ea
leeyejin <leeyejin113@gmail.com>
2024-01-12 10:43:16 +0900

    [#60] Feat: email 필드 추가

--- src/main/java/sparta/com/sappun/domain/user/dto/response/UserProfileUpdateRes.java
+++ src/main/java/sparta/com/sappun/domain/user/dto/response/UserProfileUpdateRes.java
@@ -12,14 +12,17 @@ public class UserProfileUpdateRes {
     private Long id;
     private String username;
     private String nickname;
+    private String email;
     private String profileUrl;
     // private Integer score; // 좋아요(+1) + 신고(-1)
 
     @Builder
-    private UserProfileUpdateRes(Long id, String username, String nickname, String profileUrl) {
+    private UserProfileUpdateRes(
+            Long id, String username, String nickname, String email, String profileUrl) {
         this.id = id;
         this.username = username;
         this.nickname = nickname;
+        this.email = email;
         this.profileUrl = profileUrl;
     }
 }
