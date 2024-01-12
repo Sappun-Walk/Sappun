14c0e6f61a2a7c0df19a60589c6cfc6c58eb27a4
hayul93 <parksy785@gmail.com>
2024-01-12 19:58:40 +0900

    [#71] Feat: Add S3Validator

---
+++ src/main/java/sparta/com/sappun/global/validator/S3Validator.java
@@ -0,0 +1,36 @@
+package sparta.com.sappun.global.validator;
+
+import org.springframework.web.multipart.MultipartFile;
+import sparta.com.sappun.global.exception.GlobalException;
+
+import static sparta.com.sappun.global.response.ResultCode.INVALID_PROFILE_IMAGE_FILE;
+import static sparta.com.sappun.global.response.ResultCode.NULL_FILE_TYPE;
+
+public class S3Validator {
+
+    private static final String IMAGE_JPG = "image/jpeg";
+    private static final String IMAGE_PNG = "image/png";
+
+    public static void isProfileImageFile(MultipartFile multipartFile) {
+        // 업로드된 파일의 컨텐츠 타입 가져오기
+        String fileType = multipartFile.getContentType();
+
+        // 파일 타입이 유효하지 않은 경우 예외 발생
+        if (isNullFileType(fileType)) {
+            throw new GlobalException(NULL_FILE_TYPE);
+        }
+        // 이미지 파일이 아닌 경우 예외 발생
+        if (!isImageFile(fileType)) {
+            throw new GlobalException(INVALID_PROFILE_IMAGE_FILE);
+        }
+    }
+
+    private static boolean isNullFileType(String fileType) {
+        return fileType == null;
+    }
+
+    private static boolean isImageFile(String fileType) {
+        // 파일의 컨텐츠 타입이 이미지 파일(JPEG or PNG)인지 여부를 판단
+        return fileType.equals(IMAGE_JPG) || fileType.equals(IMAGE_PNG);
+    }
+}
