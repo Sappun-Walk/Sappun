6da1b51a7484b2eb86e3df213e42ae6c951b71d7
leeyejin <leeyejin113@gmail.com>
2024-01-14 22:09:03 +0900

    [#85] Remove: 사용하지 않는 좋아요 mapper 삭제

--- src/main/java/sparta/com/sappun/domain/likeBoard/service/LikeBoardServiceMapper.java
+++
@@ -1,7 +0,0 @@
-package sparta.com.sappun.domain.likeBoard.service;
-
-import org.mapstruct.factory.Mappers;
-
-public interface LikeBoardServiceMapper {
-    LikeBoardServiceMapper INSTANCE = Mappers.getMapper(LikeBoardServiceMapper.class);
-}
--- src/main/java/sparta/com/sappun/domain/likeComment/service/LikeCommentServiceMappper.java
+++
@@ -1,7 +0,0 @@
-package sparta.com.sappun.domain.likeComment.service;
-
-import org.mapstruct.factory.Mappers;
-
-public interface LikeCommentServiceMappper {
-    LikeCommentServiceMappper INSTANCE = Mappers.getMapper(LikeCommentServiceMappper.class);
-}
