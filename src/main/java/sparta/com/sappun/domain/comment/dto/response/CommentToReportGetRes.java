82dec6b55d4a02f90a4849426757536eae9eab8b
hayul93 <parksy785@gmail.com>
2024-01-15 16:42:44 +0900

    [#90] Feat: 신고된 댓글 조회 API

---
+++ src/main/java/sparta/com/sappun/domain/comment/dto/response/CommentToReportGetRes.java
@@ -0,0 +1,34 @@
+package sparta.com.sappun.domain.comment.dto.response;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommentToReportGetRes {
+
+    private Long id;
+    private String nickname;
+    private String content;
+    private String fileURL;
+    private Integer likeCount;
+    private Integer reportCount;
+
+    @Builder
+    public CommentToReportGetRes(
+            Long id,
+            String nickname,
+            String content,
+            String fileURL,
+            Integer likeCount,
+            Integer reportCount) {
+        this.id = id;
+        this.nickname = nickname;
+        this.content = content;
+        this.fileURL = fileURL;
+        this.likeCount = likeCount;
+        this.reportCount = reportCount;
+    }
+}
