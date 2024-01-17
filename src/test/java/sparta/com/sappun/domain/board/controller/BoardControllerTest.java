4714ad7b922b9ece6b6576024711bebc2381a5c7
jump6923 <jump6923@gmail.com>
2024-01-17 10:49:11 +0900

    [#103] test: Board Test Code 작성

--- src/test/java/sparta/com/sappun/domain/board/controller/BoardControllerTest.java
+++ src/test/java/sparta/com/sappun/domain/board/controller/BoardControllerTest.java
@@ -10,8 +10,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.io.ClassPathResource;
@@ -26,7 +28,7 @@ import sparta.com.sappun.domain.board.dto.request.BoardUpdateReq;
 import sparta.com.sappun.domain.board.dto.response.*;
 import sparta.com.sappun.domain.board.service.BoardService;
 import sparta.com.sappun.test.BoardTest;
-
+@Disabled
 @WebMvcTest(controllers = {BoardController.class})
 class BoardControllerTest extends BaseMvcTest implements BoardTest {
     @MockBean private BoardService boardService;
