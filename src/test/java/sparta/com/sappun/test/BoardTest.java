e234934c4e207bcbc8005c2acef514a58f8f157a
jump6923 <jump6923@gmail.com>
2024-01-17 10:30:09 +0900

    [#114] Fix: Board 관련 오류 수정

--- src/test/java/sparta/com/sappun/test/BoardTest.java
+++ src/test/java/sparta/com/sappun/test/BoardTest.java
@@ -11,8 +11,8 @@ public interface BoardTest extends UserTest {
     String TEST_BOARD_TITLE = "TEST_TITLE";
     String TEST_BOARD_CONTENT = "TEST_CONTENT";
     String TEST_BOARD_URL = "TEST_URL";
-    String TEST_DEPARTRURE = "TEST_DEPATURE";
-    String TEST_DESTINATION = "TEST_DESTINAION";
+    String TEST_DEPARTURE = "TEST_DEPARTURE";
+    String TEST_DESTINATION = "TEST_DESTINATION";
     List<String> TEST_STOPOVER = List.of("TEST1", "TEST2", "TEST3", "TEST4", "TEST5");
     RegionEnum TEST_REGION1 = RegionEnum.REGION1;
 
@@ -26,11 +26,39 @@ public interface BoardTest extends UserTest {
                     .title(TEST_BOARD_TITLE)
                     .content(TEST_BOARD_CONTENT)
                     .fileURL(TEST_BOARD_URL)
-                    .departure(TEST_DEPARTRURE)
+                    .departure(TEST_DEPARTURE)
                     .destination(TEST_DESTINATION)
                     .stopover(TEST_STOPOVER)
                     .region(TEST_REGION1)
                     .likeCount(TEST_LIKECOUNT)
+                    .reportCount(TEST_REPORTCOUNT)
+                    .build();
+
+    Board TEST_BOARD1 =
+            Board.builder()
+                    .user(TEST_USER)
+                    .title(TEST_BOARD_TITLE)
+                    .content(TEST_BOARD_CONTENT)
+                    .fileURL(TEST_BOARD_URL)
+                    .departure(TEST_DEPARTURE)
+                    .destination(TEST_DESTINATION)
+                    .stopover(TEST_STOPOVER)
+                    .region(TEST_REGION1)
+                    .likeCount(1)
+                    .reportCount(TEST_REPORTCOUNT)
+                    .build();
+
+    Board TEST_BOARD2 =
+            Board.builder()
+                    .user(TEST_USER)
+                    .title(TEST_BOARD_TITLE)
+                    .content(TEST_BOARD_CONTENT)
+                    .fileURL(TEST_BOARD_URL)
+                    .departure(TEST_DEPARTURE)
+                    .destination(TEST_DESTINATION)
+                    .stopover(TEST_STOPOVER)
+                    .region(TEST_REGION1)
+                    .likeCount(2)
                     .reportCount(TEST_REPORTCOUNT)
                     .build();
 }
