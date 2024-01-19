cd534c832f7a800d8ac98a971156ad803b068150
leeyejin <leeyejin113@gmail.com>
2024-01-19 13:11:27 +0900

    [#146] test: CommentRepositoryTest 추가

--- src/test/java/sparta/com/sappun/domain/comment/repository/CommentRepositoryTest.java
+++ src/test/java/sparta/com/sappun/domain/comment/repository/CommentRepositoryTest.java
@@ -2,14 +2,19 @@ package sparta.com.sappun.domain.comment.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
+import sparta.com.sappun.domain.board.entity.Board;
 import sparta.com.sappun.domain.board.repository.BoardRepository;
 import sparta.com.sappun.domain.comment.entity.Comment;
+import sparta.com.sappun.domain.user.entity.User;
 import sparta.com.sappun.domain.user.repository.UserRepository;
 import sparta.com.sappun.test.CommentTest;
 
@@ -21,6 +26,8 @@ class CommentRepositoryTest implements CommentTest {
     @Autowired private CommentRepository commentRepository;
     @Autowired private UserRepository userRepository;
     @Autowired private BoardRepository boardRepository;
+
+    @PersistenceContext EntityManager em;
 
     @Test
     @DisplayName("save 테스트")
@@ -64,6 +71,34 @@ class CommentRepositoryTest implements CommentTest {
         // when
         // 데이터베이스에서 삭제
         commentRepository.delete(comment);
+        Comment findComment = commentRepository.findById(comment.getId());
+
+        // then
+        assertNull(findComment);
+    }
+
+    @Test
+    @DisplayName("deleteAllByUser 테스트")
+    @Transactional
+    void deleteAllByUserTest() {
+        // given
+        User user = userRepository.save(TEST_USER);
+        Board board = boardRepository.save(TEST_BOARD);
+        Comment comment =
+                Comment.builder()
+                        .content(TEST_COMMENT_CONTENT)
+                        .fileURL(TEST_COMMENT_FILEURL)
+                        .user(user)
+                        .board(board)
+                        .likeCount(TEST_LIKE_COUNT)
+                        .reportCount(TEST_REPORT_COUNT)
+                        .build();
+        commentRepository.save(comment);
+
+        // when
+        commentRepository.deleteAllByUser(user);
+        em.flush(); // 변경사항을 데이터베이스에 즉시 반영
+        em.clear(); // 영속성 컨텍스트 초기화
         Comment findComment = commentRepository.findById(comment.getId());
 
         // then
