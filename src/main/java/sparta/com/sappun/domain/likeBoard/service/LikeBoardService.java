a6021b252e2be42dc46d6720dcacbf85dce8cefb
leeyejin <leeyejin113@gmail.com>
2024-01-23 12:46:12 +0900

    [#262] Feat: 좋아요 응답값 수정

--- src/main/java/sparta/com/sappun/domain/likeBoard/service/LikeBoardService.java
+++ src/main/java/sparta/com/sappun/domain/likeBoard/service/LikeBoardService.java
@@ -27,17 +27,20 @@ public class LikeBoardService {
         UserValidator.validate(user);
         Board board = boardRepository.findById(boardId);
         BoardValidator.validate(board);
+        LikeBoardSaveRes res;
 
         if (likeBoardRepository.existsLikeBoardByBoardAndUser(board, user)) { // 이미 좋아요를 누른 상태라면
             board.getUser().updateScore(-10); // 좋아요를 받은 게시글의 작성자 점수 -10
             likeBoardRepository.deleteLikeBoardByBoardAndUser(board, user); // 좋아요 삭제
             board.clickLikeBoard(-1);
+            res = LikeBoardSaveRes.builder().isLiked(false).build();
         } else { // 좋아요를 안 누른 상태라면
             board.getUser().updateScore(10); // 좋아요를 받은 게시글의 작성자 점수 +10
             likeBoardRepository.save(LikeBoard.builder().board(board).user(user).build()); // 좋아요 저장
             board.clickLikeBoard(1);
+            res = LikeBoardSaveRes.builder().isLiked(true).build();
         }
 
-        return new LikeBoardSaveRes();
+        return res;
     }
 }
