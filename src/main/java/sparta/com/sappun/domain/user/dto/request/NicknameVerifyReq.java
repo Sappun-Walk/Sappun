574c46f9d79c10195d214d39cf285236cc2d1ef4
rlawogks22 <rlawogks1234@gmail.com>
2024-01-25 15:48:10 +0900

    [#298] feat : 닉네임 입력시 필요 설명 알림창 작성

--- src/main/java/sparta/com/sappun/domain/user/dto/request/NicknameVerifyReq.java
+++ src/main/java/sparta/com/sappun/domain/user/dto/request/NicknameVerifyReq.java
@@ -9,7 +9,7 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NicknameVerifyReq {
     @Size(min = 2, max = 10, message = "최소 2글자, 최대 10글자로 입력해주세요")
-    @Pattern(regexp = "^[가-힣a-zA-Z0-9]*$" , message = "닉네임에 특수문자는 사용할 수 없습니다.")
+    @Pattern(regexp = "^[가-힣a-zA-Z0-9]*$", message = "닉네임에 특수문자는 사용할 수 없습니다.")
     private String nickname;
 
     @Builder
