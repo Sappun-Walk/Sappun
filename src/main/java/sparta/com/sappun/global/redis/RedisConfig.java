2c3e84f4a65059a4d060e96aff4256998566ec5c
rlawogks22 <rlawogks1234@gmail.com>
2024-02-02 16:46:00 +0900

    [#409] refactor : 정적파일 분리

--- src/main/java/sparta/com/sappun/global/redis/RedisConfig.java
+++ src/main/java/sparta/com/sappun/global/redis/RedisConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -20,23 +19,23 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
-    @Value("${spring.data.redis.password}")
-    private String password;
-
-    //    @Bean
-    //    public RedisConnectionFactory redisConnectionFactory() {
-    //        return new LettuceConnectionFactory(host, port);
-    //    }
+    //    @Value("${spring.data.redis.password}")
+    //    private String password;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-        config.setHostName(host);
-        config.setPort(port);
-        config.setPassword(password);
-
-        return new LettuceConnectionFactory(config);
+        return new LettuceConnectionFactory(host, port);
     }
+
+    //    @Bean
+    //    public RedisConnectionFactory redisConnectionFactory() {
+    //        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+    //        config.setHostName(host);
+    //        config.setPort(port);
+    //        config.setPassword(password);
+    //
+    //        return new LettuceConnectionFactory(config);
+    //    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
