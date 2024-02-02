226a7476c690c3aa846524cbe9be099c69b2d431
rlawogks22 <rlawogks1234@gmail.com>
2024-02-02 19:30:00 +0900

    [#411] refactor : 정적파일 분리

--- src/main/java/sparta/com/sappun/global/redis/RedisConfig.java
+++ src/main/java/sparta/com/sappun/global/redis/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -19,32 +20,32 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
-    //    @Value("${spring.data.redis.password}")
-    //    private String password;
+        @Value("${spring.data.redis.password}")
+        private String password;
 
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
-    }
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory() {
+//        return new LettuceConnectionFactory(host, port);
+//    }
 
-    //    @Bean
-    //    public RedisConnectionFactory redisConnectionFactory() {
-    //        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-    //        config.setHostName(host);
-    //        config.setPort(port);
-    //        config.setPassword(password);
-    //
-    //        return new LettuceConnectionFactory(config);
-    //    }
+        @Bean
+        public RedisConnectionFactory redisConnectionFactory() {
+            RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+            config.setHostName(host);
+            config.setPort(port);
+            config.setPassword(password);
 
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        return redisTemplate;
-    }
+            return new LettuceConnectionFactory(config);
+        }
+
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate() {
+//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+//        redisTemplate.setConnectionFactory(redisConnectionFactory());
+//        redisTemplate.setKeySerializer(new StringRedisSerializer());
+//        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+//        return redisTemplate;
+//    }
 
     @Bean
     public StringRedisTemplate stringRedisTemplate() {
